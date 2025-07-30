323e9a37a0a8e965e40a98f4de6eb165af4a70bf
zukash <48586209+zukash@users.noreply.github.com>
2025-07-31 02:51:52 +0900

    feat: VSCodeのキーバインディングと設定を更新
    - keybindings.json: 不要なターミナルトグルのキーバインディングを削除し、新しいショートカットを追加
    - settings.json: Zenモードの通知設定を追加
    - functions.zsh: fzf-z-search関数のelseブロックのインデントを修正

--- installer.sh
+++ installer.sh
@@ -8,4 +8,5 @@ git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.a
 
 stow -R zsh -t $HOME
 stow -R vscode -t "$HOME/Library/Application Support/Code/User"
+cat vscode/extensions.txt | xargs -n 1 code --install-extension
 stow -R config -t $HOME/.config
