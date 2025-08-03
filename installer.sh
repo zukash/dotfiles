c064f5253c82cd4392889a561d40d0aee7ce5843
zukash <48586209+zukash@users.noreply.github.com>
2025-08-04 08:57:48 +0900

    feat: tmux設定とVSCode設定の更新
    - tmux.conf: Sキーでペインの同期を設定
    - installer.sh: VSCode拡張機能のインストール手順を修正
    - settings.json: zenMode.showTabs設定を削除

--- installer.sh
+++ installer.sh
@@ -5,9 +5,9 @@ brew install stow
 
 # https://github.com/mattmc3/antidote
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.antidote
-cat vscode/extensions.txt | xargs -n 1 code --install-extension
 
 stow -R zsh -t $HOME
 stow -R vscode -t "$HOME/Library/Application Support/Code/User"
-cat vscode/extensions.txt | xargs -n 1 code --install-extension
+#  code --list-extensions > ./vscode/extensions.txt
+cat ./vscode/extensions.txt | xargs -n 1 code --install-extension
 stow -R config -t $HOME/.config
