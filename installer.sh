c8b65ee13dc67e1a35e14472eb658efac660098c
zukash <48586209+zukash@users.noreply.github.com>
2025-08-10 13:14:31 +0900

    feat: MCP設定ファイルを追加し、VSCode拡張機能リストを更新
    - config/mcp/mcp.json: 新規作成、MCPサーバーのターゲット設定を追加
    - installer.sh: コメントアウトされたインストールコマンドを整理
    - vscode/extensions.txt: 拡張機能リストに新しい拡張機能を追加し、不要なものを削除

--- installer.sh
+++ installer.sh
@@ -1,10 +1,9 @@
-brew install fzf
-
-brew install stow
+# brew install fzf
+# brew install stow
 # apt install stow
-
 # https://github.com/mattmc3/antidote
-git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.antidote
+# git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.antidote
+# mcpctl: https://github.com/zukash/mcpctl
 
 stow -R zsh -t $HOME
 stow -R vscode -t "$HOME/Library/Application Support/Code/User"
