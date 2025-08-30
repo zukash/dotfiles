ff6c8c9af105e226acbb201ae55f383f020c30d3
zukash <48586209+zukash@users.noreply.github.com>
2025-08-31 01:17:29 +0900

    feat: 設定ファイルを追加し、tmuxとnvimの設定を更新
    - installer.sh: karabiner, mcpctl, nvim, tmuxの設定を追加
    - karabiner.json: 新規作成
    - mcp.json: 新規作成
    - init.vim: 新規作成
    - tmux.conf: 新規作成
    - keybindings.json: 不要なショートカットを削除

--- installer.sh
+++ installer.sh
@@ -9,7 +9,10 @@ stow -R zsh -t $HOME
 stow -R vscode -t "$HOME/Library/Application Support/Code/User"
 #  code --list-extensions > ./vscode/extensions.txt
 cat ./vscode/extensions.txt | xargs -n 1 code --install-extension
-stow -R config -t "$HOME/.config"
+stow -R karabiner -t "$HOME/.config/karabiner"
+stow -R mcpctl -t "$HOME/.config/mcpctl"
+stow -R nvim -t "$HOME/.config/nvim"
+stow -R tmux -t "$HOME/.config/tmux"
 stow -R hammerspoon -t "$HOME/.hammerspoon"
 
 # https://github.com/zukash/mcpctl
