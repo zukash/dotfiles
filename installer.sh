5efd0ddbd42d34bb0c105ce214a388bc4eafb730
zukash <48586209+zukash@users.noreply.github.com>
2025-11-29 23:52:02 +0900

    feat: installer.shからmcpctlの設定を削除
    - mcpctlに関するコメントアウトされた行を削除
    - mcpctlのstowコマンドを削除
    - mcpctlの適用コマンドを削除

--- installer.sh
+++ installer.sh
@@ -3,19 +3,14 @@
 # apt install stow
 # https://github.com/mattmc3/antidote
 # git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.antidote
-# mcpctl: https://github.com/zukash/mcpctl
 
 stow -R zsh -t $HOME
 stow -R vscode -t "$HOME/Library/Application Support/Code/User"
 #  code --list-extensions > ./vscode/extensions.txt
 cat ./vscode/extensions.txt | xargs -n 1 code --install-extension
 stow -R karabiner -t "$HOME/.config/karabiner"
-stow -R mcpctl -t "$HOME/.config/mcpctl"
 stow -R nvim -t "$HOME/.config/nvim"
 stow -R tmux -t "$HOME/.config/tmux"
 stow -R hammerspoon -t "$HOME/.hammerspoon"
 stow -R opencode -t "$HOME/.config/opencode"
 stow -R ghostty -t "$HOME/.config/ghostty"
-
-# https://github.com/zukash/mcpctl
-mcpctl apply -f ~/.config/mcpctl/mcp.json
