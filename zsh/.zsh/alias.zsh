f6ecb21c77a301b7b6714a8c76ae9db71b274b63
zukash <48586209+zukash@users.noreply.github.com>
2025-11-30 11:58:26 +0900

    feat: lsエイリアスを--color=autoに変更
    - zsh/.zsh/alias.zsh: lsコマンドのエイリアスを'ls -G'から'ls --color=auto'に変更

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -1,5 +1,5 @@
 # Aliases
-alias ls='ls -G'
+alias ls='ls --color=auto'
 alias gcd='cd $(ghq root)/$(ghq list | fzf-tmux -p 80%)'
 alias mockcat='while true; do echo -e "HTTP/1.1 200 OK\n\n$(date)" | nc -l 1337; done'
 alias ting='afplay /System/Library/Sounds/Blow.aiff'
